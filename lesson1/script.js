86c581c730947eadb8bca0e1f33fac25d45644de
Serjant <greatserjant007@gmail.com>
2019-12-13 13:50:07 +0700

    первый коммит

---
+++ lesson1/script.js
@@ -0,0 +1,21 @@
+//let a = null;
+
+// console.warn(a, console);
+// console.log(typeof a);
+
+// confirm("Are you here?");
+
+// let answer = prompt("Есть ли вам 18", "Yes");
+
+//  console.log(typeof(answer));
+
+// первое числа 12.3, infinity, NaN - число / на строку 
+// второе это строка "Пример"
+// третье это булево значение True/false
+// тип null этого значения нет
+// тип undefined это значение не задано но переменая есть
+// символ Symbol()
+
+//console.log("arr" + " - object");
+
+console.log("3" == 2);
